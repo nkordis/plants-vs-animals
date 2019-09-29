d8868a85124e194ba4d2ff5855d5f38fe0ccdc9e
Nikos <nikoskordis@gmail.com>
2019-09-29 11:32:07 +0200

    Initial commit

---
+++ Assets/Scripts/Boar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boar : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+
+        if(otherObject.GetComponent<Thorn>())
+        {
+            GetComponent<Animator>().SetTrigger("jumpTrigger");
+        }
+
+        else if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+    }
+
+}
