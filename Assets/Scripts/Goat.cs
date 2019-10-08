b519cfe2ad3975f17459a270e124fbee38189255
Nikos <nikoskordis@gmail.com>
2019-10-08 09:51:26 +0200

    Added new attacker and level 16

---
+++ Assets/Scripts/Goat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goat : MonoBehaviour {
+
+	private float damage = 500f;
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+
+		if (otherObject.GetComponent<Mushroom>()) {
+			GetComponent<Attacker>().Attack(otherObject);
+			GetComponent<Attacker>().StrikeCurrentTarget(damage);
+			GetComponent<Health>().DealDamage(damage);
+		}
+
+        else if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+			GetComponent<Attacker>().StrikeCurrentTarget(damage);
+        }
+    }
+
+}
